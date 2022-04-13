219441d6549efb8c4482424d5b30f4f2899f7a47
akonst17 <akonst17@gmail.com>
2022-04-13 11:58:30 +0300

    upgrade; build 3

--- NCE/Classes/Service/Stats/Share/StatsShareManager.swift
+++ NCE/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.nce.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.nce.widget")
     }
     
     @discardableResult
